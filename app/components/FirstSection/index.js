209f1d8b3805c3538a4083a2f4e8016e7b8bc12a
anto__dev <antonidwm@gmail.com>
2023-07-08 20:12:13 +0100

    ADD section first component

--- app/components/FirstSection/index.js
+++ app/components/FirstSection/index.js
@@ -1,28 +1,39 @@
-import React from 'react'
-import ButtonPage from '../ButtonPage'
+import React from "react";
+import ButtonPage from "../ButtonPage";
+import "./FirstSection.scss";
 
-const FirstSection = ({firstTxt, nameTitle, linkBtn, nameBtn, urlImg}) => {
+const FirstSection = ({ firstTxt, nameTitle, linkBtn, nameBtn, urlImg }) => {
   return (
     <>
-    <section className="sect__first">
+      <section className="sect__first">
         <div className="sect__colGauche">
-            <div className="colGauche__boxTitre">
-                <h1 className="sect__title">
-                    Le monde fe gioia {nameTitle}
-                </h1>
-            </div>
+          <div className="colGauche__boxTitre">
+            <h1 className="sect__title">
+              {nameTitle} <span className="colGauche__titleEl">Monde</span>
+              <span className="colGauche__titleEl">Gioia</span>
+            </h1>
+            <span className="sect__title colGauche__smallEl">
+              du <br /><br />
+              de
+            </span>
+          </div>
 
-            <p className="sect__txt">
-                {firstTxt}
-            </p>
+          <div className="colGauche__boxContent">
+            <p className="sect__txt">{firstTxt}</p>
             <ButtonPage linkBtn={linkBtn} nameBtn={nameBtn} />
+          </div>
         </div>
 
-        <div className="sect__colDroite"><img src={urlImg} alt="Présentation de chats de la chatteries." className="sect__img" /></div>
-    </section>
-    
+        <div className="sect__colDroite">
+          <img
+            src={urlImg}
+            alt="Présentation de chats de la chatteries."
+            className="sect__img"
+          />
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default FirstSection
+export default FirstSection;
--- app/page.js
+++ app/page.js
@@ -3,10 +3,15 @@ import FirstSection from "./components/FirstSection";
 
 
 export default function Home() {
+
+  let allTxt = [
+    <>Nous sommes un élevages familliale, où nos chats, des <b>British Shorthair</b>, vivents en notre compagnies. </>,
+
+]
   return (
     <>
       <div className="home">
-        <FirstSection firstTxt={'je suis home'} nameTitle={"Chatterie"} nameBtn={"Voir les chats"} linkBtn={'/noschats'} urlImg={'https://images.unsplash.com/photo-1495360010541-f48722b34f7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=736&q=80'} />
+        <FirstSection firstTxt={allTxt[0]} nameTitle={"Chatterie"} nameBtn={"Voir les chats"} linkBtn={'/noschats'} urlImg={'https://images.unsplash.com/photo-1495360010541-f48722b34f7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=736&q=80'} />
         <ButtonPage nameBtn={"Voir les chats"} linkBtn={'/noschats'} />
       </div>
     </>
