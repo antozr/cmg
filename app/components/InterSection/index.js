bd049f1b9cb27817f851c78da555a5d95483effb
anto__dev <antonidwm@gmail.com>
2023-07-10 22:28:38 +0100

    ADD section inter component

---
+++ app/components/InterSection/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ButtonPage from '../ButtonPage'
+import './style.scss';
+
+const InterSection = ({titleInter, nameBtn, linkBtn}) => {
+  return (
+    <>
+        <div className="sect__inter">
+            <h2 className="sect__title sect__title--big">
+                {titleInter}
+            </h2>
+            <ButtonPage nameBtn={nameBtn} linkBtn={linkBtn}/>
+        </div>
+    </>
+  )
+}
+
+export default InterSection
--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
 import ButtonPage from "./components/ButtonPage";
 import FirstSection from "./components/FirstSection";
+import InterSection from "./components/InterSection";
 
 
 export default function Home() {
 
+  let allTitle = [
+    <>Une envie de boules de poils</>
+  ]
   let allTxt = [
     <>Nous sommes un élevages familliale, où nos chats, des <b>British Shorthair</b>, vivents en notre compagnies. </>,
 
@@ -12,7 +16,7 @@ export default function Home() {
     <>
       <div className="home">
         <FirstSection firstTxt={allTxt[0]} nameTitle={"Chatterie"} nameBtn={"Voir les chats"} linkBtn={'/noschats'} urlImg={'https://images.unsplash.com/photo-1495360010541-f48722b34f7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=736&q=80'} />
-        <ButtonPage nameBtn={"Voir les chats"} linkBtn={'/noschats'} />
+        <InterSection titleInter={allTitle[0]} nameBtn={" Voir les chattons"} linkBtn={'/noschattons'} />
       </div>
     </>
   )
